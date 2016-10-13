1ecc48e394012f97d9547fa639d7e33af67ed5b4
jzbdota <yangyi19901228@gmail.com>
2016-10-13 15:07:49 -0700

    merged the branch,advanced code

--- test2.py
+++ test2.py
@@ -4,28 +4,12 @@
 
 i = int(raw_input('net interest:'))
 
-overhead = 0
-
-if i > 10:
-	overhead = 1
-	if i > 20:
-		overhead = overhead + 10 * .075
-		if i > 40:
-			overhead = overhead + 20 * .05
-			if i > 60:
-				overhead = overhead + 20 *.03
-				if i > 100:
-					overhead = overhead + 40 * .015
-					overhead = overhead + (i-100) * .01
-				else :
-					overhead = overhead + (i-60) * .015
-			else :
-				overhead = overhead + (i-40)* .03
-		else :
-			overhead = overhead + (i-20)* .05
-	else :
-		overhead = overhead + (i-10)* .075
-else : 
-	overhead = i*.1
-	
-print overhead
+arr = [100, 60, 40, 20, 10, 0]
+rat = [.01, .015, .03, .05, .075, .1]
+r = 0
+for idx in range(0,6):
+	if i >arr[idx]:
+		r+=(i-arr[idx])*rat[idx]
+		i=arr[idx]
+		
+print r
